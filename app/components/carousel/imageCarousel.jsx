5c5712b8c53e3331b3f4f12aee91ce1b08edca23
Dimitry <dimitriko@elad.co.il>
2025-03-02 15:29:58 +0200

    added product carousel

--- app/components/carousel/imageCarousel.jsx
+++ app/components/carousel/imageCarousel.jsx
@@ -1,30 +1,43 @@
-import React, { useState } from 'react';
-import { View, Image, Dimensions, StyleSheet } from 'react-native';
+import React, { useState, useRef } from 'react';
+import { View, Image, Dimensions, StyleSheet, TouchableOpacity } from 'react-native';
 import Carousel from 'react-native-reanimated-carousel';
 
 const { width } = Dimensions.get('window');
 
-const ImageCarousel = ({images}) => {
+const ImageCarousel = ({ images }) => {
     const [activeIndex, setActiveIndex] = useState(0);
+    const carouselRef = useRef(null);
+
+    const handlePress = (index) => {
+        setActiveIndex(index);
+        if (carouselRef.current) {
+            carouselRef.current.scrollTo({ index, animated: true });
+        }
+    };
 
     return (
         <View>
             <Carousel
+                ref={carouselRef}
                 loop
                 width={width}
                 height={width}
                 data={images}
                 scrollAnimationDuration={1000}
-                onSnapToItem={index => setActiveIndex(index)}
+                onSnapToItem={setActiveIndex}
                 renderItem={({ item }) => (
                     <View style={styles.imageContainer}>
-                        <Image source={{ uri: item.url }} style={styles.image} resizeMode="cover" sourceType="image/png" />
+                        <Image source={{ uri: item.url }} style={styles.image} resizeMode="cover" />
                     </View>
                 )}
             />
             <View style={styles.paginationContainer}>
                 {images.map((_, index) => (
-                    <View key={index} style={[styles.dot, activeIndex === index && styles.activeDot]} />
+                    <TouchableOpacity
+                        key={index}
+                        style={[styles.dot, activeIndex === index && styles.activeDot]}
+                        onPress={() => handlePress(index)}
+                    />
                 ))}
             </View>
         </View>
@@ -36,9 +49,9 @@ const styles = StyleSheet.create({
     image: { width: width, height: width, backgroundColor: 'white' },
     paginationContainer: { flexDirection: 'row', justifyContent: 'center', paddingTop: 30 },
     dot: {
-        width: 10,
-        height: 10,
-        borderRadius: 5,
+        width: 12,
+        height: 12,
+        borderRadius: 6,
         borderWidth: 1,
         borderColor: '#858585',
         backgroundColor: '#f1f2ed',
