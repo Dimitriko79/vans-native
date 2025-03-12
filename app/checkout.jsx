4d4078bad60a17a0d943b6c39f88a327ed8d66d5
Dimitry <dimitriko@elad.co.il>
2025-03-12 10:32:57 +0200

    update logic of checkout

--- app/checkout.jsx
+++ app/checkout.jsx
@@ -1,13 +1,14 @@
 import {View, Text, StyleSheet, ScrollView} from "react-native";
 import useCheckout, {CHECKOUT_STEP} from "./components/checkout/useCheckout";
 import RichContent from "./components/richContent/richContent";
-import React from "react";
+import React, {useRef} from "react";
 import ProgressBar from "./components/checkout/progressBar/progressBar";
 import FormDetails from "./components/checkout/formDetails/formDetails";
 import ItemReview from "./components/checkout/itemsReview/itemsReview";
 import Error from "./components/error/error";
 import FormPayment from "./components/checkout/formPayment/formPayment";
 import useCheckoutContext from "./context/checkout/checkoutProvider";
+import PlaceOrder from "./components/checkout/placeOrder/placeOrder";
 
 const Checkout = () => {
 
@@ -26,12 +27,8 @@ const Checkout = () => {
     } = useCheckout();
 
     const {
-        shippingDetails,
-        cart_id,
         amount,
-        payment,
         products,
-        order_number
     } = useCheckoutContext();
 
     let content;
@@ -42,13 +39,27 @@ const Checkout = () => {
                 <View style={styles.checkout_content_title}>
                     <Text style={styles.checkout_content_title_text}>{CHECKOUT_STEP[step].title}</Text>
                 </View>
-                <FormDetails step={step} handleStep={handleStep} handleCustomerDetails={handleCustomerDetails} shippingMethods={shippingMethods} isStepOneDone={isStepOneDone} setStepOneDone={setStepOneDone}/>
+                <FormDetails
+                    step={step}
+                    handleStep={handleStep}
+                    handleCustomerDetails={handleCustomerDetails}
+                    shippingMethods={shippingMethods}
+                    isStepOneDone={isStepOneDone}
+                    setStepOneDone={setStepOneDone}
+                />
             </View>
         )
     } else if(CHECKOUT_STEP[step].id === 2) {
         content = (
             <View style={styles.checkout_content}>
-                <FormDetails step={step} handleStep={handleStep} handleCustomerDetails={handleCustomerDetails} shippingMethods={shippingMethods} isStepOneDone={isStepOneDone} setStepOneDone={setStepOneDone}/>
+                <FormDetails
+                    step={step}
+                    handleStep={handleStep}
+                    handleCustomerDetails={handleCustomerDetails}
+                    shippingMethods={shippingMethods}
+                    isStepOneDone={isStepOneDone}
+                    setStepOneDone={setStepOneDone}
+                />
                 <View style={styles.checkout_content_title}>
                     <Text style={styles.checkout_content_title_text}>{CHECKOUT_STEP[step].title}</Text>
                 </View>
@@ -58,7 +69,7 @@ const Checkout = () => {
     } else if (CHECKOUT_STEP[step].id === 3) {
         content = (
             <View style={styles.checkout_content}>
-
+                <PlaceOrder/>
             </View>
         )
     } else {
@@ -66,15 +77,17 @@ const Checkout = () => {
     }
 
     return (
-        <ScrollView keyboardShouldPersistTaps="handled">
             <View style={styles.container}>
                 <Error errorMessage={errorMessage} onErrorMessage={onErrorMessage}/>
-                {cmsBlockData && cmsBlockData.length && cmsBlockData.map((item, index) =><View key={index}><RichContent html={item?.content || ''}/></View> )}
-                <ProgressBar step={step}/>
+                {CHECKOUT_STEP[step].id !== 3 && (
+                    <>
+                        {cmsBlockData && cmsBlockData.length && cmsBlockData.map((item, index) =><View key={index}><RichContent html={item?.content || ''}/></View> )}
+                        <ProgressBar step={step}/>
+                        <ItemReview totalPrice={CHECKOUT_STEP[step].id !== 3 ? totalPrice : amount?.grand_total} productList={CHECKOUT_STEP[step].id !== 3 ? productList : products}/>
+                    </>
+                )}
                 {content}
-                <ItemReview totalPrice={CHECKOUT_STEP[step].id !== 3 ? totalPrice : amount?.grand_total} productList={CHECKOUT_STEP[step].id !== 3 ? productList : products}/>
             </View>
-        </ScrollView>
     )
 }
 
@@ -100,7 +113,7 @@ const styles = StyleSheet.create({
         fontSize: 16,
         fontWeight: 600,
         color: '#333'
-    },
+    }
 })
 
 export default Checkout;
