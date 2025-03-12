94313a9ee587a3a10e66c4970f97b68baa59bd86
Dimitry <dimitriko@elad.co.il>
2025-03-12 11:09:39 +0200

    update logic of checkout

--- app/components/popularyProduct/popularyProduct.jsx
+++ app/components/popularyProduct/popularyProduct.jsx
@@ -1,127 +1,127 @@
-import {View, StyleSheet, Text, Dimensions, Image, TouchableOpacity, FlatList} from "react-native";
-import {Link} from "expo-router";
+import { View, StyleSheet, Text, Dimensions, Image, FlatList } from "react-native";
+import { Link } from "expo-router";
 import formatImageUrl from "../../helpers/formatImageUrl";
-import React from "react";
+import React, { useMemo } from "react";
 
 const { width } = Dimensions.get("window");
 
-const PopularyProduct = ({data}) => {
+const PopularProduct = ({ data = { title: "", popular_products: [] } }) => {
+    if (!data?.popular_products?.length) return null;
 
-    if (!data) return null;
-
-    const {title, popular_products} = data;
+    const { title, popular_products } = data;
+    const products = useMemo(() => popular_products, [popular_products]);
 
     return (
-        <View style={styles.popylary}>
-            <View style={styles.popylary_title_wrapper}>
-                <Text style={styles.popylary_title_before}/>
-                <Text style={styles.popylary_title}>{title}</Text>
-                <Text style={styles.popylary_title_after}/>
+        <View style={styles.popular}>
+            <View style={styles.popular_title_wrapper}>
+                <Text style={styles.popular_title_before} />
+                <Text style={styles.popular_title}>{title}</Text>
+                <Text style={styles.popular_title_after} />
             </View>
             <FlatList
-                data={popular_products}
-                keyExtractor={(item, index) => index}
+                data={products}
+                keyExtractor={(item) => item.url}
                 numColumns={2}
                 renderItem={({ item }) => (
-                    <View style={styles.populary_product}>
-                        <Link href={item.url} style={styles.populary_product_link} target="_blank">
-                            <Image style={styles.populary_product_image} resizeMode="cover" source={{uri: formatImageUrl(item.image)}} />
-                            <View style={styles.populary_product_text_wrapper}>
-                                <Text style={styles.populary_product_text}>{item.title}</Text>
-                                <Text style={styles.populary_product_price}>{item.price}</Text>
+                    <View style={styles.popular_product}>
+                        <Link href={item.url} style={styles.popular_product_link} target="_blank">
+                            <Image
+                                style={styles.popular_product_image}
+                                resizeMode="cover"
+                                source={{ uri: formatImageUrl(item.image) }}
+                            />
+                            <View style={styles.popular_product_text_wrapper}>
+                                <Text style={styles.popular_product_text}>{item.title}</Text>
+                                <Text style={styles.popular_product_price}>{item.price}</Text>
                             </View>
                         </Link>
                     </View>
                 )}
-                contentContainerStyle={styles.populary_products}
+                contentContainerStyle={styles.popular_products}
                 scrollEnabled={false}
                 nestedScrollEnabled={false}
             />
         </View>
-    )
-}
+    );
+};
 
 const styles = StyleSheet.create({
-    popylary: {
+    popular: {
         flex: 1,
-        backgroundColor: '#f1f2ed',
+        backgroundColor: "#f1f2ed",
         marginTop: 40,
         marginBottom: 10,
         paddingBottom: 40,
-        marginRight: 10,
-        marginLeft: 10
+        marginHorizontal: 10,
     },
-    popylary_title_wrapper: {
-        display: "flex",
+    popular_title_wrapper: {
         flexDirection: "row",
         alignItems: "center",
         justifyContent: "space-between",
         marginTop: 50,
-        paddingRight: 10,
-        paddingLeft: 10
+        paddingHorizontal: 10,
     },
-    popylary_title: {
+    popular_title: {
         textAlign: "center",
         fontSize: 26,
         letterSpacing: -1.5,
         fontWeight: "bold",
     },
-    popylary_title_before: {
+    popular_title_before: {
         height: 5,
-        width:50,
+        width: 50,
         borderTopWidth: 1,
         borderBottomWidth: 1,
-        borderStyle: "solid",
-        borderColor: '#c1c2bd'
+        borderColor: "#c1c2bd",
     },
-    popylary_title_after: {
+    popular_title_after: {
         height: 5,
-        width:60,
+        width: 60,
         borderTopWidth: 1,
         borderBottomWidth: 1,
-        borderStyle: "solid",
-        borderColor: '#c1c2bd'
+        borderColor: "#c1c2bd",
     },
-    populary_products: {
-        marginTop: 20
+    popular_products: {
+        marginTop: 20,
     },
-    populary_product: {
+    popular_product: {
         flex: 1,
         margin: 5,
-        width: width / 2,
+        width: width / 2 - 10,
+        borderRadius: 0,
+        elevation: 3,
     },
-    populary_product_link: {
+    popular_product_link: {
         flex: 1,
-        display: "flex",
-        width: width / 2,
+        width: "100%",
         flexDirection: "column",
-        justifyContent: "flex-start",
         alignItems: "center",
     },
-    populary_product_image: {
-        width: (width / 2) - 20,
+    popular_product_image: {
+        width: "100%",
         height: width / 2,
-        aspectRatio: 1
+        aspectRatio: 1,
+        borderRadius: 0,
     },
-    populary_product_text_wrapper: {
-        width: (width / 2) - 20,
+    popular_product_text_wrapper: {
+        width: "100%",
         flexDirection: "column",
-        paddingTop: 10
+        paddingTop: 10,
+        alignItems: "center",
     },
-    populary_product_text: {
+    popular_product_text: {
         color: "#000",
         fontSize: 14,
-        height: 40,
         fontWeight: "normal",
         textAlign: "center",
+        marginBottom: 5,
     },
-    populary_product_price: {
+    popular_product_price: {
         color: "#000",
         fontSize: 12,
         fontWeight: "bold",
         textAlign: "center",
-        display: "flex",
-    }
-})
+    },
+});
 
-export default PopularyProduct;
+export default PopularProduct;
